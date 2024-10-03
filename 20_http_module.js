0b3659058cff752f7a7dfd41ab9d084450e77e02
shubhamg <shubhamg200204@gmail.com>
2024-10-03 15:05:43 +0530

    a

--- 20_http_module.js
+++ 20_http_module.js
@@ -2,7 +2,7 @@ const http = require('http');
 // http also extends EventEmitter
 
 const server = http.createServer((req, res) => {    // here (req, res) => is event listener call back function 
-    res.writeHead(200);
+    res.writeHead(200, { "Content-Type" : "text/plain" });
     res.end("Hello World!");
 })
 
